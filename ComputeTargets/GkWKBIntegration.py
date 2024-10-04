More fixes for WKB calculation

ComputeTargets/GkWKBIntegration.py
@@ -5,17 +5,12 @@ import ray
 from math import log, sqrt, fabs, cos, sin
 from scipy.integrate import solve_ivp
 
-from ComputeTargets import IntegrationSolver
 from ComputeTargets.WKB_tensor_Green import WKB_omegaEff_sq, WKB_d_ln_omegaEffPrime_dz
 from ComputeTargets.analytic_Gk import (
     compute_analytic_G,
     compute_analytic_Gprime,
 )
-from ComputeTargets.integration_supervisor import (
-    RHS_timer,
-    IntegrationSupervisor,
-    DEFAULT_UPDATE_INTERVAL,
-)
+from ComputeTargets.integration_metadata import IntegrationSolver
 from CosmologyConcepts import wavenumber_exit_time, redshift, redshift_array, wavenumber
 from CosmologyModels import BaseCosmology
 from Datastore import DatastoreObject
@@ -26,6 +21,11 @@ from defaults import (
     DEFAULT_REL_TOLERANCE,
 )
 from utilities import check_units, format_time
+from .integration_supervisor import (
+    RHS_timer,
+    IntegrationSupervisor,
+    DEFAULT_UPDATE_INTERVAL,
+)
 
 A0_TAU_INDEX = 0
 THETA_INDEX = 1