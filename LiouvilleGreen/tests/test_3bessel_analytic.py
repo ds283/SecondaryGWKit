fb72b04f685f3eb61890967b0a751be42027dbe4
ds283 <D.Seery@sussex.ac.uk>
2025-01-17 12:07:19 +0000

    Adjust YJJ unit tests to assess convergence to the asymptotic formula for realistic values of k and eta (and hence x = k eta)
    - want to check whether the integral has converged to the Kohri & Terada kernel estimate by the current cosmological epoch

--- LiouvilleGreen/tests/test_3bessel_analytic.py
+++ LiouvilleGreen/tests/test_3bessel_analytic.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from datetime import datetime
 from pathlib import Path
@@ -9,11 +10,17 @@ from matplotlib import pyplot as plt
 
 from LiouvilleGreen.bessel_phase import bessel_phase
 from LiouvilleGreen.three_bessel_integrals import quad_JJJ, quad_YJJ
+from utilities import format_time
 
 ABS_TOLERANCE = 1e-6
 REL_TOLERANCE = 1e-5
 
-MAX_X = 1e8
+SINGULARITY_ABS_TOLERANCE = 1e-3
+SINGULARITY_REL_TOLERANCE = 1e-2
+
+# with max eta around 10500 and a largest k of order 10^7/Mpc or so, the largest argument we would
+# appear to need is about 1E4 * 1E7 ~ 1E11
+MAX_X = 1e12
 
 
 def is_triangle(k: float, q: float, s: float):
@@ -32,10 +39,20 @@ def plot_and_compute_3Bessel(
     analytic_result: float,
     label: str,
     timestamp,
+    phase_atol=1e-25,
+    phase_rtol=5e-14,
+    quad_atol=1e-14,
+    quad_rtol=1e-10,
 ):
-    mu_phase = bessel_phase(mu + 0.5, 1.075 * k * max_x, atol=1e-25, rtol=5e-14)
-    nu_phase = bessel_phase(nu + 0.5, 1.075 * q * max_x, atol=1e-25, rtol=5e-14)
-    sigma_phase = bessel_phase(sigma + 0.5, 1.075 * s * max_x, atol=1e-25, rtol=5e-14)
+    mu_phase = bessel_phase(
+        mu + 0.5, 1.075 * k * max_x, atol=phase_atol, rtol=phase_rtol
+    )
+    nu_phase = bessel_phase(
+        nu + 0.5, 1.075 * q * max_x, atol=phase_atol, rtol=phase_rtol
+    )
+    sigma_phase = bessel_phase(
+        sigma + 0.5, 1.075 * s * max_x, atol=phase_atol, rtol=phase_rtol
+    )
 
     x_grid = np.logspace(np.log10(100.0), np.log10(max_x), 250)
     y_grid = [
@@ -50,8 +67,8 @@ def plot_and_compute_3Bessel(
             q,
             s,
             x,
-            atol=1e-14,
-            rtol=1e-10,
+            atol=quad_atol,
+            rtol=quad_rtol,
         )
         for x in x_grid
     ]
@@ -100,8 +117,8 @@ def plot_and_compute_3Bessel(
         q,
         s,
         max_x,
-        atol=1e-14,
-        rtol=1e-10,
+        atol=quad_atol,
+        rtol=quad_rtol,
     )
     return value
 
@@ -281,6 +298,7 @@ class Y022:
 
 Jintegrals = [J000, J110, J220, J222, J231]
 Yintegrals = [Y000, Y022]
+singularity_eps = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10]
 
 
 def intify(x: float):
@@ -373,41 +391,199 @@ class Test3BesselAnalytic(unittest.TestCase):
             print(f"   relerr={relerr:.5g}, abserr={abserr:.5g}")
             self.assertTrue(relerr < REL_TOLERANCE or abserr < ABS_TOLERANCE)
 
-    def test_J110_notriangle(self):
-        J = J110
-        k = 4.870969802124623
-        q = 1.3338040554375765
-        s = 2.0319743925393587
+    def test_YJJ_log_singularity(self):
+        timestamp = datetime.now().replace(microsecond=0)
 
-        analytic = J.analytic(k, q, s)
+        for Y in Yintegrals:
+            k = uniform(0.1, 5.0)
+            q = uniform(0.1, 5.0)
 
-        max_x = MAX_X
-        mu_phase = bessel_phase(J.mu + 0.5, 1.075 * k * max_x, atol=1e-25, rtol=5e-14)
-        nu_phase = bessel_phase(J.nu + 0.5, 1.075 * q * max_x, atol=1e-25, rtol=5e-14)
-        sigma_phase = bessel_phase(
-            J.sigma + 0.5, 1.075 * s * max_x, atol=1e-25, rtol=5e-14
-        )
+            for eps in singularity_eps:
+                s_lo = np.abs(k - q) + eps
+                s_hi = k + q - eps
 
-        numeric = quad_JJJ(
-            mu_phase,
-            nu_phase,
-            sigma_phase,
-            J.mu,
-            J.nu,
-            J.sigma,
-            k,
-            q,
-            s,
-            max_x,
-            atol=1e-14,
-            rtol=1e-10,
-        )
+                values = [("lo", s_lo), ("hi", s_hi)]
 
-        abserr = np.fabs(numeric - analytic)
+                for label, s in values:
+                    analytic = Y.analytic(k, q, s)
+                    numeric = plot_and_compute_3Bessel(
+                        quad_YJJ,
+                        Y.mu,
+                        Y.nu,
+                        Y.sigma,
+                        k,
+                        q,
+                        s,
+                        max_x=MAX_X,
+                        analytic_result=analytic,
+                        label=f"YJJ_log_eps={eps:.2g}_{label}",
+                        timestamp=timestamp,
+                        quad_atol=1e-10,
+                        quad_rtol=1e-8,
+                    )
 
-        print(f"@@ ({intify(J.mu)},{intify(J.nu)},{intify(J.sigma)}):")
-        print(f"   k={k}, q={q}, s={s}, is_triangle={is_triangle(k, q, s)}")
-        print(f"   quadrature result = {numeric}")
-        print(f"   analytic result = {analytic}")
-        print(f"   abserr={abserr:.5g}")
-        self.assertTrue(abserr < ABS_TOLERANCE)
+                    abserr = np.fabs(numeric - analytic)
+                    relerr = abserr / analytic
+
+                    print(
+                        f"@@ (Y{intify(Y.mu)},J{intify(Y.nu)},J{intify(Y.sigma)}) eps={eps:.2g} {label}:"
+                    )
+                    if is_triangle(k, q, s):
+                        print(
+                            f"   k={k}, q={q}, s={s} satisfies the triangle inequality"
+                        )
+                    else:
+                        print(
+                            f"   k={k}, q={q}, s={s} does not satisfy the triangle inequality"
+                        )
+
+                    print(f"   quadrature result = {numeric}")
+                    print(f"   analytic result = {analytic}")
+                    print(f"   relerr={relerr:.5g}, abserr={abserr:.5g}")
+                    self.assertTrue(
+                        relerr < SINGULARITY_REL_TOLERANCE
+                        or abserr < SINGULARITY_ABS_TOLERANCE
+                    )
+
+    def test_YJJ_log_scaling(
+        self,
+        max_x=MAX_X,
+        phase_atol=1e-25,
+        phase_rtol=5e-14,
+        quad_atol=1e-10,
+        quad_rtol=1e-8,
+    ):
+        timestamp = datetime.now().replace(microsecond=0)
+
+        sns.set_theme()
+
+        for Y in Yintegrals:
+            k = uniform(0.1, 5.0)
+            q = uniform(0.1, 5.0)
+
+            mu_phase = bessel_phase(
+                Y.mu + 0.5, 1.075 * k * max_x, atol=phase_atol, rtol=phase_rtol
+            )
+            nu_phase = bessel_phase(
+                Y.nu + 0.5, 1.075 * q * max_x, atol=phase_atol, rtol=phase_rtol
+            )
+
+            configs = [
+                ("lo", lambda k, q, eps: np.abs(k - q) + eps),
+                ("hi", lambda k, q, eps: k + q - eps),
+            ]
+
+            for label, s_evaluator in configs:
+                print(
+                    f"@@ (Y{intify(Y.mu)},J{intify(Y.nu)},J{intify(Y.sigma)}) {label}:"
+                )
+
+                total_start = time.perf_counter()
+                last_notify = total_start
+
+                analytic_grid = []
+                numeric_grid = []
+
+                for eps in singularity_eps:
+                    start = time.perf_counter()
+
+                    s = s_evaluator(k, q, eps)
+
+                    sigma_phase = bessel_phase(
+                        Y.sigma + 0.5,
+                        1.075 * s * max_x,
+                        atol=phase_atol,
+                        rtol=phase_rtol,
+                    )
+
+                    analytic = Y.analytic(k, q, s)
+                    numeric = quad_YJJ(
+                        mu_phase,
+                        nu_phase,
+                        sigma_phase,
+                        Y.mu,
+                        Y.nu,
+                        Y.sigma,
+                        k,
+                        q,
+                        s,
+                        max_x,
+                        atol=quad_atol,
+                        rtol=quad_rtol,
+                    )
+
+                    analytic_grid.append(analytic)
+                    numeric_grid.append(numeric)
+
+                    stop = time.perf_counter()
+                    eps_period = stop - start
+                    last_notify_period = stop - last_notify
+                    if eps_period > 2 * 60 or last_notify_period > 5 * 60:
+                        print(
+                            f"   -- evaluated eps={eps:.3g} (value={numeric:.5g}) in time {format_time(eps_period)} (total time for this YJJ {format_time(last_notify_period)})"
+                        )
+                        last_notify = stop
+
+                fig = plt.figure()
+                ax = plt.gca()
+
+                ax.plot(
+                    singularity_eps, numeric_grid, color="b", label="Numeric + Levin"
+                )
+                ax.plot(
+                    singularity_eps,
+                    analytic_grid,
+                    color="r",
+                    linestyle=(0, (1, 1)),
+                    label="Analytic",
+                )
+
+                ax.text(
+                    0.0, 1.03, f"k={k:.5g}", transform=ax.transAxes, fontsize="x-small"
+                )
+                ax.text(
+                    0.3, 1.03, f"q={q:.5g}", transform=ax.transAxes, fontsize="x-small"
+                )
+                ax.text(
+                    0.6, 1.03, f"s={s:.5g}", transform=ax.transAxes, fontsize="x-small"
+                )
+                ax.text(
+                    0.0,
+                    1.08,
+                    f"$\\mu$={Y.mu:.5g}",
+                    transform=ax.transAxes,
+                    fontsize="x-small",
+                )
+                ax.text(
+                    0.3,
+                    1.08,
+                    f"$\\nu$={Y.nu:.5g}",
+                    transform=ax.transAxes,
+                    fontsize="x-small",
+                )
+                ax.text(
+                    0.6,
+                    1.08,
+                    f"$\\sigma$={Y.sigma:.5g}",
+                    transform=ax.transAxes,
+                    fontsize="x-small",
+                )
+
+                ax.set_xscale("log")
+                ax.set_yscale("linear")
+                ax.legend(loc="best")
+                ax.grid(True)
+
+                fig_path = Path(
+                    f"test_3bessel_analytic/{timestamp.isoformat()}/eps_dependence_{label}_mu={Y.mu:.3f}_nu={Y.nu:.3f}_sigma={Y.sigma:.3f}_k={k:.3f}_q={q:.3f}_s={s:.3f}_maxx={max_x:.5g}.pdf"
+                ).resolve()
+                fig_path.parents[0].mkdir(parents=True, exist_ok=True)
+                fig.savefig(fig_path)
+                fig.savefig(fig_path.with_suffix(".png"))
+
+                plt.close()
+
+                total_stop = time.perf_counter()
+                total_elapsed = total_stop - total_start
+
+                print(f"   >> completed plot in time {format_time(total_elapsed)}")
